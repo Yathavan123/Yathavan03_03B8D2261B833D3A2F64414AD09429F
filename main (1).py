6aa4dfd0456297d66796999609c7cc7e78882566
Yathavan123 <144442205+Yathavan123@users.noreply.github.com>
2023-09-20 12:41:37 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,8 +1,37 @@
-# Get the year from the user
-year = int(input("Enter a year: "))
+class Bankaccount:
 
-# Check if it's a leap year
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(year, "is a leap year")
-else:
-    print(year, "is not a leap year")
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited ₹{}. New balance:  ₹{}".format(amount, 
+                                                       self.__account_balance))
+    else:
+      print("Invalid deposit amount.")
+
+  def withdrawl(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+       self.__account_balance -= amount
+       print("Withdrew  ₹{}. New balance: ₹{}" .format(amount, 
+                                                       self.__account_balance))
+    else:
+       print("Invalid withdrawl amount or insufficient balance.")
+
+  def display_balance(self):
+     print("Account balance for {} (Account #{}): ₹{}" .format(
+        self.__account_holder_name, self.__account_number,
+        self.__account_balance))
+
+
+account = Bankaccount(account_number="123456789",
+                      account_holder_name="Bharath Kumar" ,
+                      initial_balance=5000.0)
+#Text deposit and withdrawl functionality]
+account.deposit(500.0)
+account.withdrawl(200.0)
+account.display_balance()
+
--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-def factorial(n):
-    if n == 0:
-        return 1
+class Bankaccount:
+
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited ₹{}. New balance:  ₹{}".format(amount, 
+                                                       self.__account_balance))
     else:
-        return n * factorial(n - 1)
+      print("Invalid deposit amount.")
 
-# Input the number
-number = int(input("Enter a number: "))
+  def withdrawl(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+       self.__account_balance -= amount
+       print("Withdrew  ₹{}. New balance: ₹{}" .format(amount, 
+                                                       self.__account_balance))
+    else:
+       print("Invalid withdrawl amount or insufficient balance.")
 
-# Calculate the factorial using the recursive function
-result = factorial(number)
+  def display_balance(self):
+     print("Account balance for {} (Account #{}): ₹{}" .format(
+        self.__account_holder_name, self.__account_number,
+        self.__account_balance))
 
-# Print the result
-print(f"The factorial of {number} is {result}")
+
+account = Bankaccount(account_number="123456789",
+                      account_holder_name="Bharath Kumar" ,
+                      initial_balance=5000.0)
+#Text deposit and withdrawl functionality]
+account.deposit(500.0)
+account.withdrawl(200.0)
+account.display_balance()
